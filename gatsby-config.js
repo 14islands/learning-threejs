37401f960174274d19bdf41619f91594a5040fbd
Vitor Dino <vitor@kunstdesign.com.br>
2020-07-31 18:19:19 -0300

    :construction_worker: deploy to github page script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // repo name
+  pathPrefix: "/learning-threejs",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
